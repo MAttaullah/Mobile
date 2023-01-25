0a64db97a6b2103d918786e4dfd84c28bdd6c3de
MAttaullah <attaullahkhan3521@gmail.com>
2023-01-25 07:40:33 +0500

    finish

--- app/src/main/java/com/example/assign_mad/MainActivity.java
+++ app/src/main/java/com/example/assign_mad/MainActivity.java
@@ -2,13 +2,16 @@ package com.example.assign_mad;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SearchView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.relative);
+
     }
 }
